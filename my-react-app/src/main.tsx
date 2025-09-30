30f7fb5be571c069741566174c565b6506c03d4f
Brian O'Donnell <bodonne3@depaul.edu>
2025-09-30 00:14:26 +0000

    custom hooks

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -1,15 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import MultiApp from './components/multiComponentPage.tsx'
-import PropApp from './components/PropApp.tsx'
-import UserCard from './components/EmbeddedProps.tsx'
-import Counter from './components/Counter.tsx'
+// import App from './App.tsx'
+// import MultiApp from './components/multiComponentPage.tsx'
+// import PropApp from './components/PropApp.tsx'
+// import UserCard from './components/EmbeddedProps.tsx'
+// import Counter from './components/Counter.tsx'
+import CustomHookExamples from './components/CustomHookExamples.tsx'
 
 
 
 createRoot(document.getElementById('root')!).render(
     <>
-    <Counter  />
+    <CustomHookExamples />
     </>
 )
